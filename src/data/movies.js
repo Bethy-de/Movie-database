af952aee53b96668686329fec3443debd25baad4
Bethelhem Degsew Alem <bethydegsew@gmail.com>
2025-12-12 04:18:29 +0300

    Update movies.js

--- src/data/movies.js
+++ src/data/movies.js
@@ -1,1 +1,14 @@
 
+const movies = [
+  {
+    id: 1,
+    title: "Inception",
+    genre: "Sci-Fi",
+    rating: 8.8,
+    description: "A mind-bending thriller by Christopher Nolan.",
+    image: "https://m.media-amazon.com/images/I/51nbVEuw1HL._AC_SY445_.jpg",
+  },
+  // Add more movies here
+];
+
+export default movies;
